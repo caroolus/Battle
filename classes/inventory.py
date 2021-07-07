45915723cc2f0d5957e4c84f2e7662673f8beae3
caroolus <55993717+caroolus@users.noreply.github.com>
2021-07-07 17:32:39 +0200

    Add files via upload

---
+++ classes/inventory.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self, name, type, description, prop):
+        self.type = type
+        self.description = description
+        self.prop = prop
+        self.name = name
+
+
